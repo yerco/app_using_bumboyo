6b43e90ac41d99a41f79d274d953032e01a54d5c
Yerco <yerco@optmail.com>
2022-02-21 18:32:38 -0300

    Create and delete boooks

--- app.py
+++ app.py
@@ -2,12 +2,13 @@
 
 from bumboyo.api import API
 
-from auth import STATIC_TOKEN
+from auth import login_required, TokenMiddleware, STATIC_TOKEN
 from storage import BookStorage
 
 app = API()
 book_storage = BookStorage()
 book_storage.create(name="7 habits of highly effective people", author="Stephen Covey")
+app.add_middleware(TokenMiddleware)
 
 @app.route("/", allowed_methods=["get"])
 def index(req, resp):
@@ -17,3 +18,18 @@ def index(req, resp):
 @app.route("/login", allowed_methods=["post"])
 def login(req, resp):
     resp.json = {"token": STATIC_TOKEN}
+
+@app.route("/books", allowed_methods=["post"])
+@login_required
+def create_book(req, resp):
+    book = book_storage.create(**req.POST)
+
+    resp.status_code = 201
+    resp.json = book._asdict
+
+@app.route("/books/{id:d}", allowed_methods=["delete"])
+@login_required
+def delete_book(req, resp, id):
+    book_storage.delete(id)
+
+    resp.status_code = 204
